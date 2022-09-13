40b5e389af882883c9ef706552f83c2ef11f16c4
Pedro <pbbdasilva@gmail.com>
2022-09-13 15:57:24 -0500

    Added all cuda kernels

--- src/CircusTent/Impl/CT_CUDA/CT_CUDA_IMPL.cu
+++ src/CircusTent/Impl/CT_CUDA/CT_CUDA_IMPL.cu
@@ -9,60 +9,297 @@
  */
 
 #include <stdlib.h>
+#include <iostream>
 #include <cuda.h>
 
-__global__ void RAND_ADD( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
+using namespace std;
 
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicAdd( &ARRAY[IDX[start]], (u_int64_t) 0x1 );
-}
-
-__global__ void RAND_CAS( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
-
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicCAS( &ARRAY[IDX[start]], ARRAY[IDX[start]], ARRAY[IDX[start]]);
-}
-
-__global__ void STRIDE1_ADD( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
+__global__ void RAND_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicAdd( &ARRAY[start], (u_int64_t) 0x1 );
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicAdd( (unsigned long long int *) &ARRAY[IDX[i]], (unsigned long long int) 0x1 );
+    }
 }
 
-__global__ void STRIDE1_CAS( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
+__global__ void RAND_CAS( uint64_t* __restrict__ ARRAY, uint64_t *IDX, uint64_t iters, uint64_t pes ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicCAS( &ARRAY[start], ARRAY[start], ARRAY[start]);
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicCAS( (unsigned long long int *) &ARRAY[IDX[i]], (unsigned long long int) ARRAY[IDX[i]], (unsigned long long int) ARRAY[IDX[i]]);
+    }
 }
 
-__global__ void STRIDEN_ADD( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes, uint64_t stride ) {
+__global__ void STRIDE1_ADD( uint64_t* __restrict__ ARRAY, uint64_t iters, uint64_t pes ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters && start % stride == 0 )
-        atomicAdd( &ARRAY[start], (u_int64_t) 0x1 );
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicAdd( (unsigned long long int *) &ARRAY[i], (unsigned long long int) 0x1 );
+    }
 }
 
-__global__ void STRIDEN_CAS( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes, uint64_t stride ) {
+__global__ void STRIDE1_CAS( uint64_t* __restrict__ ARRAY, uint64_t iters, uint64_t pes ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters && start % stride == 0 )
-        atomicCAS( &ARRAY[start], ARRAY[start], ARRAY[start]);
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicCAS( (unsigned long long int *) &ARRAY[i], (unsigned long long int) ARRAY[i], (unsigned long long int) ARRAY[i] );
+    }
 }
 
-__global__ void CENTRAL_ADD( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
+__global__ void STRIDEN_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes, uint64_t stride ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicAdd( &ARRAY[0], (u_int64_t) 0x1 );
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i += stride ) {
+        atomicAdd( (unsigned long long int *) &ARRAY[i], (unsigned long long int) 0x1 );
+    }
 }
 
-__global__ void CENTRAL_CAS( u_int64_t *restrict ARRAY, u_int64_t *restrict IDX, u_int64_t iters, u_int64_t pes ) {
+__global__ void STRIDEN_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes, uint64_t stride ) {
     
-    u_int64_t start = threadIdx.x + blockIdx.x * blockDim.x;
-    if( start < iters)
-        atomicCAS( &ARRAY[0], ARRAY[start], ARRAY[start]);
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+	uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = start; i < (start + iters_per_thread); i += stride ) {
+        atomicCAS( (unsigned long long int *) &ARRAY[i], (unsigned long long int) ARRAY[i], (unsigned long long int) ARRAY[i] );
+    }
+}
+
+__global__ void CENTRAL_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes ) {
+    
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = 0; i < iters_per_thread; i++ ) {
+        atomicAdd( (unsigned long long int *) &ARRAY[0], (unsigned long long int) 0x1 );
+    }
+}
+
+__global__ void CENTRAL_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes ) {
+    
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = 0; i < iters_per_thread; i++ ) {
+        atomicCAS( (unsigned long long int *) &ARRAY[0], (unsigned long long int) ARRAY[0], (unsigned long long int) ARRAY[0] );
+    }
+}
+
+__global__ void PTRCHASE_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = 0; i < iters_per_thread; i++ ) {
+        start = atomicAdd( (unsigned long long int *) &IDX[start], (unsigned long long int) 0x0 );
+    }
+}
+
+__global__ void PTRCHASE_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    
+    uint64_t i;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread; //@TO-DO ask if should be 0
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    for( i = 0; i < iters_per_thread; i++ ) {
+        atomicCAS( (unsigned long long int *) &start, (unsigned long long int) start, (unsigned long long int) IDX[start] );
+    }
+}
+
+__global__ void SG_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    
+    uint64_t i;
+    uint64_t src, dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    src =  0x0;
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        src = atomicAdd( (unsigned long long int *) &IDX[i], (unsigned long long int) 0x0 );
+        dest = atomicAdd( (unsigned long long int *) &IDX[i+1], (unsigned long long int) 0x0 );
+        val = atomicAdd( (unsigned long long int *) &ARRAY[src], (unsigned long long int) 0x0 );
+        atomicAdd( (unsigned long long int *) &ARRAY[dest], (unsigned long long int) val );
+    }
+}
+
+__global__ void SG_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    
+    uint64_t i;
+    uint64_t src, dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    src =  0x0;
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicCAS( (unsigned long long int *) &src, (unsigned long long int) src, (unsigned long long int) IDX[i] );
+        atomicCAS( (unsigned long long int *) &dest, (unsigned long long int) dest, (unsigned long long int) IDX[i+1] );
+        atomicCAS( (unsigned long long int *) &val, (unsigned long long int) val, (unsigned long long int) ARRAY[src] );
+        atomicCAS( (unsigned long long int *) &ARRAY[dest], (unsigned long long int) ARRAY[dest], (unsigned long long int) val );
+    }
+}
+
+__global__ void SCATTER_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    uint64_t i;
+    uint64_t dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        dest = atomicAdd( (unsigned long long int *) &IDX[i+1], (unsigned long long int) 0x0 );
+        val = atomicAdd( (unsigned long long int *) &ARRAY[i], (unsigned long long int) 0x1 );
+        atomicAdd( (unsigned long long int *) &ARRAY[dest], (unsigned long long int) val );
+    }
+}
+
+__global__ void SCATTER_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    uint64_t i;
+    uint64_t dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicCAS( (unsigned long long int *) &dest, (unsigned long long int) dest, (unsigned long long int) IDX[i+1] );
+        atomicCAS( (unsigned long long int *) &val, (unsigned long long int) val, (unsigned long long int) ARRAY[i] );
+        atomicCAS( (unsigned long long int *) &ARRAY[dest], (unsigned long long int) ARRAY[dest], (unsigned long long int) val );
+    }
+}
+
+__global__ void GATHER_ADD( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    uint64_t i;
+    uint64_t dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        dest = atomicAdd( (unsigned long long int *) &IDX[i+1], (unsigned long long int) 0x0 );
+        val = atomicAdd( (unsigned long long int *) &ARRAY[dest], (unsigned long long int) 0x1 );
+        atomicAdd( (unsigned long long int *) &ARRAY[i], (unsigned long long int) val );
+    }
+}
+
+__global__ void GATHER_CAS( uint64_t* __restrict__ ARRAY, uint64_t* __restrict__ IDX, uint64_t iters, uint64_t pes) {
+    uint64_t i;
+    uint64_t dest, val;
+    uint64_t num_threads = (uint64_t) blockDim.x * gridDim.x;
+    uint64_t iters_per_thread = iters/num_threads;
+    uint64_t start = (threadIdx.x + blockIdx.x * blockDim.x) * iters_per_thread;
+
+	if(threadIdx.x == blockDim.x - 1 && blockIdx.x == gridDim.x - 1) {
+		iters_per_thread += iters % num_threads;
+	}
+
+    dest = 0x0;
+    val =  0x0;
+
+    for( i = start; i < (start + iters_per_thread); i++ ) {
+        atomicCAS( (unsigned long long int *) &dest, (unsigned long long int) dest, (unsigned long long int) IDX[i+1] );
+        atomicCAS( (unsigned long long int *) &val, (unsigned long long int) val, (unsigned long long int) ARRAY[dest] );
+        atomicCAS( (unsigned long long int *) &ARRAY[i], (unsigned long long int) ARRAY[i], (unsigned long long int) val );
+    }
 }
